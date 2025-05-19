487ed5911413d666d73737ad2253da754ae086d7
nhanduc112 <115692156+nhanduc112@users.noreply.github.com>
2025-05-19 23:39:43 +0700

    set up retrofit, newsAPI

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "ccb794715adf4b14bb5e2d7ec41c7222"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
